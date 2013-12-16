3f19e2160ab6ffde1088b34e4b98b26584c8da5b
Jacob Hooey <jacob@pompouspanda.com>
2013-12-16 13:56:16 -0500

    Added basic setup files for the project

---
+++ setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+#classifiers: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+setup(
+    name = "grocerytrends",
+    version = "0.1",
+    author = "Jacob Hooey",
+    author_email = "jacob@pompouspanda.com",
+    description = ("A Python application that takes your grocery receipts "
+                                "and analyses that data to give you trends"),
+    license = "BSD",
+    keywords = "finances trending personal",
+    url = "https://github.com/jhooey/grocery-cart-trends",
+    packages = ['grocerytrends', 'tests'],
+    long_description = read('README.md'),
+    classifiers =
+        [
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: BSD License",
+        ],
+    )
