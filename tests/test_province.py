f895c2399be003579ecb8b658723bcf0032dba31
Jacob Hooey <jacob@pompouspanda.com>
2013-12-16 16:31:22 -0500

    Added a test for the GroceryStore class

--- tests/test_province.py
+++ tests/test_province.py
@@ -2,6 +2,9 @@ from nose.tools import *
 from grocerytrends.shopping import Province
 
 def test_province():
+    print ("Testing the Province class")
+    
+    
     quebec = Province("Quebec", "QC", 13)
     
     print (quebec.name)
