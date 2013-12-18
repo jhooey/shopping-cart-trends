9cbd2edfcc86a58755d68f2d43551f8a25754145
Jacob Hooey <jacob@pompouspanda.com>
2013-12-18 11:28:10 -0500

    created a new User class

---
+++ grocerytrends/user.py
@@ -0,0 +1,10 @@
+class User(object):
+    """Docstring"""
+    
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+        self.receipts = []
+        
