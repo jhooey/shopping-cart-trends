060c28b0d112abdf8f0fcc52ae9fbf96a000cc24
Jacob Hooey <jacob@pompouspanda.com>
2013-12-23 15:10:48 -0500

    Added comments and moved the table initialization to the database.py file

--- grocerytrends/database.py
+++ grocerytrends/database.py
@@ -3,4 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///test.db', echo=True)
 
+#Use by the classes to declare a mapping
 Base = declarative_base()
+
+#This initializes all of the tables in the DB
+Base.metadata.create_all(engine)
