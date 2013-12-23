487267c207c516f5fff751021c1303001e048865
Jacob Hooey <jacob@pompouspanda.com>
2013-12-23 11:35:19 -0500

    Added database mapping to the User class

--- grocerytrends/user.py
+++ grocerytrends/user.py
@@ -1,14 +1,22 @@
-import database
+from sqlalchemy import Column, Integer, String
+from database import Base
 
-class User(object):
+
+class User(Base):
     """Docstring"""
-    
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
+
     def __init__(self, first_name, last_name, username):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.password = self.create_password()
-        self.receipts = []
         
     def __str__(self):
         return ''.join([self.first_name, ' ', self.last_name, 
