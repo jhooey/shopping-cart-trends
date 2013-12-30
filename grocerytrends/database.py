199c412ea791f5b7d3992683f2a8efafa0d9b3a8
Jacob Hooey <jacob@pompouspanda.com>
2013-12-30 11:52:16 -0500

    adjusted so that the db_init can be passed a connection string, that indicates the db instead of having it hardcoded into the application.

--- grocerytrends/database.py
+++ grocerytrends/database.py
@@ -5,12 +5,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///test.db', echo=True)
-    
 #Use by the classes to declare a mapping
 Base = declarative_base()
 
-def db_init():
+
+def db_init(conn_str):
+    engine = create_engine(conn_str, echo=True)
+    
     """Sets all the initial database requirements"""
     #This initializes all of the tables in the DB
     Base.metadata.create_all(engine)
