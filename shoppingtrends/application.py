b958e64ced3e5e265a4473275d318ee38c45afb2
Jacob Hooey <jacob@pompouspanda.com>
2014-01-14 15:51:31 -0500

    Create a new module that will house all the gui components of the application

---
+++ shoppingtrends/application.py
@@ -0,0 +1,11 @@
+import Tkinter as tk
+import ttk
+import user
+
+
+class Application(tk.Tk):
+    """Container for all frames within the application"""
+    
+    def __init__(self, session, *args, **kwargs):
+        pass
+    
