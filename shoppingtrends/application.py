b1bec0d55aa623cc87cda540ff2ac595fb68c8cf
Jacob Hooey <jhooey@gmail.com>
2014-01-22 11:36:43 -0500

    created a root window and menu

--- shoppingtrends/application.py
+++ shoppingtrends/application.py
@@ -3,9 +3,42 @@ import ttk
 import user
 
 
-class Application(tk.Tk):
+class Root(tk.Tk):
     """Container for all frames within the application"""
     
-    def __init__(self, session, *args, **kwargs):
-        pass
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        
+        #initialize menu
+        self.config(menu=MenuBar(self))
+        
+
+class MenuBar(tk.Menu):
+    def __init__(self, parent):
+        tk.Menu.__init__(self, parent)
+
+        filemenu = tk.Menu(self, tearoff=False)
+        self.add_cascade(label="File",underline=0, menu=filemenu)
+        filemenu.add_command(label="New", command=self.callback)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", underline=1, command=self.quit)
+
+        helpmenu = tk.Menu(self, tearoff=False)
+        self.add_cascade(label="Help", menu=helpmenu)
+        helpmenu.add_command(label="About...", command=self.callback)
+
+    def quit(self):
+        sys.exit(0)
     
+    def callback(self):
+        print "called the callback!"
+
+
+
+            
+class Application:
+    def __init__(self, root):
+        pass
+
+root = Root()
+root.mainloop()
